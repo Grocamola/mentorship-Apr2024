72ad2f7e98a502e16de0f0ce2c8833f09aa42c95
Faraneh Javadinejad <faraneh.javadinejad@points.com>
2024-04-09 21:05:05 -0400

    SecondComitToMaster
    tic-tac-toe is done with react

--- app-one/src/components/ticTacToe/ticTacToe.jsx
+++ app-one/src/components/ticTacToe/ticTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './ticTacToe.css';
 
@@ -7,6 +7,8 @@ const TicTacToe = () => {
     const [state, setState] = useState([[11,12,13],[21,22,23],[31,32,33]])
     const [player, setPlayer] = useState('X')
     const [winner, setWinner] = useState()
+    //better work was to save scoreboard in localStorage, I kept it simple here
+    const [scoreBoard, setScoreBoard] = useState({PlayerX: 0, PlayerO: 0})
 
     const boxSelectHandler = (a, b) => {
         if (!winner) {
@@ -24,30 +26,67 @@ const TicTacToe = () => {
     
             // Check for a winner in columns
             for (let i = 0; i < state.length; i++) {
-                if (state[0][col] === state[1][col] && state[1][col] === state[2][col]) {
-                    setWinner(state[0][col]);
-                }
+                state[0][col] === state[1][col] && state[1][col] === state[2][col] && setWinner(state[0][col]);
             }
+            // Check for a winner in rows
+            for (let i = 0; i < state.length; i++) {
+                state[row][0] === state[row][1] && state[row][1] === state[row][2] && setWinner(state[row][0]);
+            }
+            // Check X
+            state[0][0] === state[1][1] && state[1][1] === state[2][2] && setWinner(state[0][0])
+            state[0][2] === state[1][1] && state[1][1] === state[2][0] && setWinner(state[0][2])
     
             setPlayer(player === 'X' ? 'O' : 'X');
             console.table(state);
         }
     };
+
+    // back to empty tic-tac-toe
+    const resetHandler = () => { 
+        setState([[11, 12, 13], [21, 22, 23], [31, 32, 33]]);
+        setPlayer(winner && winner);
+        setWinner();
+    }
+
+    // I could put this inside the handler, I used useEffect cause it seemed more organize
+    useEffect(() => {
+        if (winner === 'X') {
+            setScoreBoard(prevScore => ({
+                ...prevScore,
+                PlayerX: prevScore.PlayerX + 1
+            }));
+        } else if (winner === 'O') {
+            setScoreBoard(prevScore => ({
+                ...prevScore,
+                PlayerO: prevScore.PlayerO + 1
+            }));
+        }
+    },[winner])
     
 
 
     return ( 
         <div className="main_container">
-            <div className="messageBox">
-                {winner && <h2>{`Winner is ${winner}`}</h2>}
-            </div>
+            {winner && <div className="messageBox">
+                <h2>{`Winner is ${winner}`}</h2>
+                <button onClick={resetHandler}>RESET</button>
+            </div>}
+            
             <div className="tictactoe_boxes">
+                
                 {state.map(row => row.map(box => <div 
                     className="tictactoe_box" 
                     key={Math.random(8)} 
                     onClick={() => boxSelectHandler(row, box)}>
                         {isNaN(box) && box}
                     </div>))}
+
+                {!winner && <div className="playerName">
+                    <p>{`Player ${player}'s turn`}</p>
+                </div>}
+                <div className="scoreBoard">
+                    <p>{`Player X: ${scoreBoard.PlayerX} - Player O: ${scoreBoard.PlayerO}`}</p>
+                </div>
             </div>
         </div>
      );
